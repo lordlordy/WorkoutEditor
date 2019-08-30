478655bed4d196c5cfc0cffd71a57ba00af210a7
Steven Thomas Lord <stevenlord@me.com>
2019-08-30 11:44:51 +0100

    Initial Commit

---
+++ WorkoutEditor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WorkoutEditor
+//
+//  Created by Steven Lord on 30/08/2019.
+//  Copyright © 2019 Steven Lord. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
